03adea2039e4e2cfe43d0ec3a420150587dfdd20
M JUNAID BEG <junaidbeg96@gmail.com>
2026-08-13 20:47:23 +0530

    Auto-create users table on server start

--- database.js
+++ database.js
@@ -1,5 +1,16 @@
-const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./users.db');
 
-const db = new sqlite3.Database("users.db");
+// Ensure the table exists on startup
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      email TEXT UNIQUE,
+      password TEXT
+    )
+  `);
+});
 
 module.exports = db;
