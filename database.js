2f88339cd0ff3fb3f4067222213b3a48d53b5041
M JUNAID BEG <junaidbeg96@gmail.com>
2026-08-13 21:07:22 +0530

    Update database configuration and create users table on startup

--- database.js
+++ database.js
@@ -1,16 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./users.db');
 
-// Ensure the table exists on startup
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT,
-      email TEXT UNIQUE,
-      password TEXT
-    )
-  `);
-});
 
 module.exports = db;
